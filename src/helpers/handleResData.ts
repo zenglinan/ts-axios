03dd8e679ad56a6d95851eb0bb67dffc3650d27c
datou <1392767180@qq.com>
2020-01-15 14:06:51 +0800

    feat: 将响应的字符串数据转成JSON

---
+++ src/helpers/handleResData.ts
@@ -0,0 +1,11 @@
+// 尝试将字符串返回数据转成 JSON
+export function handleResData(data: any): any {
+  if (typeof data === 'string') {
+    try {
+      data = JSON.parse(data)
+    } catch (e) {
+      // do nothing
+    }
+  }
+  return data
+}
