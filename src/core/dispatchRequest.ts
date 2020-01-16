6f59aeb350fe8fb95fa7d82985c89e99e534946f
datou <1392767180@qq.com>
2020-01-17 00:34:42 +0800

    feat: 将 txios 扩展为混合对象

---
+++ src/core/dispatchRequest.ts
@@ -0,0 +1,26 @@
+import { TxiosRequestConfig, TxiosPromise, TxiosResponse } from '../types/index'
+import { handleUrl } from '../helpers/handleUrl'
+import xhr from './xhr'
+import { handleData } from '../helpers/handleData'
+import { handleHeaders } from '../helpers/handleHeaders'
+import { handleResData } from '../helpers/handleResData'
+
+function txios(config: TxiosRequestConfig): TxiosPromise {
+  handleConfig(config)
+  return xhr(config).then(res => {
+    handleResponseData(res)
+    return res
+  })
+}
+
+function handleConfig(config: TxiosRequestConfig) {
+  config.url = handleUrl(config.url!, config.params)
+  config.headers = handleHeaders(config.headers, config.data)
+  config.data = handleData(config.data)
+}
+
+function handleResponseData(res: TxiosResponse) {
+  res.data = handleResData(res.data)
+}
+
+export default txios
